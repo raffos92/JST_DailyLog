78ec1d88f593234e3e4e9401ec62af7c24950d0c
raffos92 <34335704+raffos92@users.noreply.github.com>
2019-02-25 10:01:27 +0100

    add project on repository

---
+++ DailyLog/DailyLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyLog
+//
+//  Created by Raff Miraglia on 2/22/19.
+//  Copyright © 2019 Raffaele Miraglia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
